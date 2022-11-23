4aebbf2f268c6046b903eda3abfa8eb2334fe525
Karine Pereira <karinesp89@gmail.com>
2022-11-23 14:37:01 +0100

    add searchBar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import './global.css';
+import './forms.css';
 import { Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { CreateList } from './pages/CreateList';
-
-
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
 
-
     <div className="App">
-
-
+      <Toaster />
       <Routes>
 
         <Route path='/' element={<Home />} />
         <Route path='/create' element={<CreateList />} />
 
       </Routes>
-
 
     </div>
   );
