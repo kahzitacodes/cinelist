42b242e46d7385036155a8340969bd135a0a8fb7
Felipeenoronha <felipecesar.rj@gmail.com>
2022-11-24 22:01:25 +0000

    atualização home

--- src/components/CardRead/index.js
+++ src/components/CardRead/index.js
@@ -3,19 +3,15 @@ import style from "./style.module.css";
 export function CardRead(props) {
   return (
     <div className={style.container}>
-
+      <img className={style.img} src={props.img} alt="foto" />
       
-      <div>
+      <div className={style.containerSecond}>
         <h2 className={style.nameList}>{props.nameList}</h2>
-
         <p className={style.year}>{props.year}</p>
-
         <p className={style.description}>{props.description}</p>
       </div>
 
-      {/* <h3 className={style.director}>{props.director}</h3> */}
-
-      <img className={style.img} src={props.img} alt="foto" />
+     
     </div>
   );
 }
