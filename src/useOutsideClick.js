fbd83f708fd221ef218ef9a3e1af040880b773cf
Karine Pereira <karinesp89@gmail.com>
2023-02-06 17:57:25 -0300

    cleaning code cor outsideclick method

--- src/useOutsideClick.js
+++ src/useOutsideClick.js
@@ -2,18 +2,16 @@ import { useEffect } from "react";
 
 export function useOutsideClick(ref, callback) {
    useEffect(() => {
-      /**
-       * Alert if clicked on outside of element
-       */
+
       function handleClickOutside(event) {
          if (ref.current && !ref.current.contains(event.target)) {
             callback();
          }
       }
-      // Bind the event listener
+
       document.addEventListener("mousedown", handleClickOutside);
       return () => {
-         // Unbind the event listener on clean up
+
          document.removeEventListener("mousedown", handleClickOutside);
       };
    }, [callback, ref]);
